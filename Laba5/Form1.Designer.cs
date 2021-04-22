c747ec26d22750cc5b2893782e535df6a690beaf
Etobasa <82803548+Etobasa@users.noreply.github.com>
2021-04-22 09:40:41 +0300

    Sending C# project

---
+++ Laba5/Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿
+namespace Laba5
+{
+    partial class Table
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Cake = new System.Windows.Forms.Button();
+            this.Bottle = new System.Windows.Forms.Button();
+            this.Blue_button = new System.Windows.Forms.Button();
+            this.Colour_text = new System.Windows.Forms.Label();
+            this.Red_button = new System.Windows.Forms.Button();
+            this.Purple_button = new System.Windows.Forms.Button();
+            this.Dish_text = new System.Windows.Forms.Label();
+            this.Savee = new System.Windows.Forms.Button();
+            this.Loadd = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Cake
+            // 
+            this.Cake.BackColor = System.Drawing.Color.Gold;
+            this.Cake.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Cake.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.Cake.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Cake.ForeColor = System.Drawing.Color.ForestGreen;
+            this.Cake.Location = new System.Drawing.Point(139, 265);
+            this.Cake.Name = "Cake";
+            this.Cake.Size = new System.Drawing.Size(75, 25);
+            this.Cake.TabIndex = 0;
+            this.Cake.Text = "Cake";
+            this.Cake.UseVisualStyleBackColor = false;
+            this.Cake.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Bottle
+            // 
+            this.Bottle.BackColor = System.Drawing.Color.ForestGreen;
+            this.Bottle.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Bottle.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.Bottle.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Bottle.ForeColor = System.Drawing.Color.Gold;
+            this.Bottle.Location = new System.Drawing.Point(281, 265);
+            this.Bottle.Name = "Bottle";
+            this.Bottle.Size = new System.Drawing.Size(75, 25);
+            this.Bottle.TabIndex = 1;
+            this.Bottle.Text = "Bottle";
+            this.Bottle.UseVisualStyleBackColor = false;
+            this.Bottle.Click += new System.EventHandler(this.Bottle_Click);
+            // 
+            // Blue_button
+            // 
+            this.Blue_button.BackColor = System.Drawing.Color.Blue;
+            this.Blue_button.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Blue_button.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.Blue_button.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Blue_button.ForeColor = System.Drawing.Color.Red;
+            this.Blue_button.Location = new System.Drawing.Point(66, 86);
+            this.Blue_button.Name = "Blue_button";
+            this.Blue_button.Size = new System.Drawing.Size(75, 25);
+            this.Blue_button.TabIndex = 2;
+            this.Blue_button.Text = "Blue";
+            this.Blue_button.UseVisualStyleBackColor = false;
+            this.Blue_button.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // Colour_text
+            // 
+            this.Colour_text.AutoSize = true;
+            this.Colour_text.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Colour_text.Location = new System.Drawing.Point(167, 37);
+            this.Colour_text.Name = "Colour_text";
+            this.Colour_text.Size = new System.Drawing.Size(160, 19);
+            this.Colour_text.TabIndex = 3;
+            this.Colour_text.Text = "Choose tablecloth colour";
+            this.Colour_text.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Red_button
+            // 
+            this.Red_button.BackColor = System.Drawing.Color.Red;
+            this.Red_button.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Red_button.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.Red_button.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Red_button.ForeColor = System.Drawing.Color.LimeGreen;
+            this.Red_button.Location = new System.Drawing.Point(208, 86);
+            this.Red_button.Name = "Red_button";
+            this.Red_button.Size = new System.Drawing.Size(75, 25);
+            this.Red_button.TabIndex = 4;
+            this.Red_button.Text = "Red";
+            this.Red_button.UseVisualStyleBackColor = false;
+            this.Red_button.Click += new System.EventHandler(this.Red_button_Click);
+            // 
+            // Purple_button
+            // 
+            this.Purple_button.BackColor = System.Drawing.Color.BlueViolet;
+            this.Purple_button.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Purple_button.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.Purple_button.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Purple_button.ForeColor = System.Drawing.Color.Orange;
+            this.Purple_button.Location = new System.Drawing.Point(341, 86);
+            this.Purple_button.Name = "Purple_button";
+            this.Purple_button.Size = new System.Drawing.Size(75, 25);
+            this.Purple_button.TabIndex = 5;
+            this.Purple_button.Text = "Purple";
+            this.Purple_button.UseVisualStyleBackColor = false;
+            this.Purple_button.Click += new System.EventHandler(this.Purple_button_Click);
+            // 
+            // Dish_text
+            // 
+            this.Dish_text.AutoSize = true;
+            this.Dish_text.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Dish_text.Location = new System.Drawing.Point(155, 218);
+            this.Dish_text.Name = "Dish_text";
+            this.Dish_text.Size = new System.Drawing.Size(189, 19);
+            this.Dish_text.TabIndex = 6;
+            this.Dish_text.Text = "What would you like to taste?";
+            this.Dish_text.Click += new System.EventHandler(this.Dish_text_Click);
+            // 
+            // Savee
+            // 
+            this.Savee.BackColor = System.Drawing.Color.SandyBrown;
+            this.Savee.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Savee.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.Savee.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Savee.ForeColor = System.Drawing.Color.Aqua;
+            this.Savee.Location = new System.Drawing.Point(28, 372);
+            this.Savee.Name = "Savee";
+            this.Savee.Size = new System.Drawing.Size(75, 25);
+            this.Savee.TabIndex = 7;
+            this.Savee.Text = "Save table";
+            this.Savee.UseVisualStyleBackColor = false;
+            this.Savee.Click += new System.EventHandler(this.Button1_Click_1);
+            // 
+            // Loadd
+            // 
+            this.Loadd.BackColor = System.Drawing.Color.Aqua;
+            this.Loadd.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Loadd.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.Loadd.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Loadd.ForeColor = System.Drawing.Color.SandyBrown;
+            this.Loadd.Location = new System.Drawing.Point(28, 412);
+            this.Loadd.Name = "Loadd";
+            this.Loadd.Size = new System.Drawing.Size(75, 25);
+            this.Loadd.TabIndex = 8;
+            this.Loadd.Text = "Load table";
+            this.Loadd.UseVisualStyleBackColor = false;
+            this.Loadd.Click += new System.EventHandler(this.Loadd_Click);
+            // 
+            // Table
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 461);
+            this.Controls.Add(this.Loadd);
+            this.Controls.Add(this.Savee);
+            this.Controls.Add(this.Dish_text);
+            this.Controls.Add(this.Purple_button);
+            this.Controls.Add(this.Red_button);
+            this.Controls.Add(this.Colour_text);
+            this.Controls.Add(this.Blue_button);
+            this.Controls.Add(this.Bottle);
+            this.Controls.Add(this.Cake);
+            this.Name = "Table";
+            this.Text = "Table";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button Bottle;
+        private System.Windows.Forms.Button Blue_button;
+        private System.Windows.Forms.Button Cake;
+        private System.Windows.Forms.Label Colour_text;
+        private System.Windows.Forms.Button Red_button;
+        private System.Windows.Forms.Button Purple_button;
+        private System.Windows.Forms.Label Dish_text;
+        private System.Windows.Forms.Button Savee;
+        private System.Windows.Forms.Button Loadd;
+    }
+}
+
---
+++ Laba5/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Win32;
+
+namespace Laba5
+{
+    public partial class Table : Form
+    {
+        public Table()
+        {
+            InitializeComponent();
+            RegistryKey reg = Registry.CurrentUser.CreateSubKey(@"Laba4");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e) //Cake
+        {
+            if (MessageBox.Show("Do you want to increase the window size by 50 units?", "Eat me!",
+                MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                Height += 50;
+                Width += 50;
+            }
+        }
+
+        private void Bottle_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Do you want to reduce the window size by 50 units?", "Drink me!", MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question) == DialogResult.Yes && this.Height > 50)
+            {
+                Height -= 50;
+                Width -= 50;
+            }
+        }
+
+        private void Button3_Click(object sender, EventArgs e) //Blue
+        {
+            RegistryKey reg = Registry.CurrentUser.CreateSubKey(@"Laba4");
+            reg.SetValue("Color", "Blue");
+            reg.Close();
+            this.BackColor = System.Drawing.Color.Blue;
+        }
+
+        private void Red_button_Click(object sender, EventArgs e)
+        {
+            RegistryKey reg = Registry.CurrentUser.CreateSubKey(@"Laba4");
+            reg.SetValue("Color", "Red");
+            reg.Close();
+            this.BackColor = System.Drawing.Color.Red;
+        }
+
+        private void Purple_button_Click(object sender, EventArgs e)
+        {
+            RegistryKey reg = Registry.CurrentUser.CreateSubKey(@"Laba4");
+            reg.SetValue("Color", "Purple");
+            reg.Close();
+            this.BackColor = System.Drawing.Color.BlueViolet;
+        }
+
+        private void Button1_Click_1(object sender, EventArgs e) //Save
+        {
+            RegistryKey reg = Registry.CurrentUser.CreateSubKey(@"Laba4");
+            reg.SetValue("Height", Height);
+            reg.SetValue("Width", Width);
+            reg.Close();
+        }
+
+        private void Loadd_Click(object sender, EventArgs e)
+        {
+            RegistryKey reg = Registry.CurrentUser.CreateSubKey(@"Laba4");
+            if (reg != null)
+            {
+                int Height = int.Parse(reg.GetValue("Height").ToString());
+                int Width = int.Parse(reg.GetValue("Width").ToString());
+                Size = new Size(Height, Width);
+                string color = (string)reg.GetValue("Color");
+                if (color == "Purple")
+                {
+                    this.BackColor = System.Drawing.Color.BlueViolet;
+                }
+                else if (color == "Red")
+                {
+                    this.BackColor = System.Drawing.Color.Red;
+                }
+                else if (color == "Blue")
+                {
+                    this.BackColor = System.Drawing.Color.Blue;
+                }
+            }
+        }
+
+        private void Dish_text_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e) //Colour
+        {
+
+        }
+    }
+}
